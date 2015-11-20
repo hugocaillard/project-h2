c73ace67303d61099cabd9d69d6b343f18dc512b
Hugo Caillard <c.hugo.188@gmail.com>
2015-11-20 13:00:42 +0100

    Simplify

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,1 @@
-import $ from './tools.js';
 
-window.log = Function.prototype.bind.call(console.log, console);
-
-$.get('/api/', (status, data) => console.log(status, data));
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import http from 'http';
 import colors from 'colors';
 
-import resSugar from './lib/helpers/resSugar.js'
 
 let app = http.createServer(function (req, res) {
-  resSugar(res);
 });
 
 app.listen(3000, () => {

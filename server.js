444a746a2454c8410882d2367654f3512154689f
Hugo Caillard <c.hugo.188@gmail.com>
2015-11-20 13:02:14 +0100

    Serve

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import http from 'http';
 import colors from 'colors';
 
+import staticServe from './lib/helpers/static.js';
 
 let app = http.createServer(function (req, res) {
+  staticServe(req, res, './public');
 });
 
 app.listen(3000, () => {

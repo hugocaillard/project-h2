f7d7deced0998d9dc8f77e2ad29673eb0c16caef
Hugo Caillard <c.hugo.188@gmail.com>
2015-11-20 14:38:24 +0100

    Base

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,1 +1,9 @@
+var $ = {
+  sel: function(el) {
+    return document.querySelector(el);
+  }
+};
 
+var $name = $.sel('#name');
+
+console.log($name);
--- server.js
+++ server.js
@@ -3,9 +3,20 @@ import colors from 'colors';
 
 import staticServe from './lib/helpers/static.js';
 
-let app = http.createServer(function (req, res) {
+function handler(req, res) {
+  console.log(req.method);
+  console.log(req.url);
+
+  if (req.url === '/api') {
+    res.write('Hello world');
+  } else if (req.url === '/api/home') {
+    res.write('Welcome home');
+  }
+
   staticServe(req, res, './public');
-});
+}
+
+var app = http.createServer(handler);
 
 app.listen(3000, () => {
   console.log('Server running at localhost:3000/'.underline);

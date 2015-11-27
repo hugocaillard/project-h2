c4408cc1207448e5ec25f4f3ac86cf75dc5f3aa2
Hugo Caillard <c.hugo.188@gmail.com>
2015-11-27 02:45:43 +0100

    Add some front basis

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,1 +1,22 @@
-console.log('rdy');
+var $ = {
+  sel: function(el) {
+    return document.querySelector(el);
+  }
+};
+
+var $msgContainer = $.sel('.messages');
+console.log($msgContainer);
+
+// function displayMessage(msg) {
+//   var $name = document.createElement('span')
+//   $name.innerHTML = msg.name;
+//   var $content = document.createElement('span');
+//   $content.innerHTML = msg.content;
+//
+//   var $msg = document.createElement('div');
+//   $msg.classList.add('message');
+//   $msg.appendChild($name);
+//   $msg.appendChild($content);
+//   $msgContainer.appendChild($msg);
+//   $msgContainer.scrollTop = $msgContainer.scrollHeight;
+// };
